14e5af5c27b9229da56b2b3f4baa4ac286a755ee
Marco E <marco.esposito@gmail.com>
2019-04-07 13:25:59 +0200

    asyncio

--- app.py
+++ app.py
@@ -1,118 +1,120 @@
-from aiohttp import web
 from log_utils import logger
 from RPi import GPIO
-from water import Pump, FloatSwitch, AbstractPumpContext
-import asyncio
 import automationhat
-import socketio
+import asyncio
+import os
 
 if not automationhat.is_automation_hat():
     raise RuntimeError("Automation HAT is not connected")
 
-sio = socketio.AsyncServer(async_mode='aiohttp')
+
+PUMPD_INTERVAL = os.environ.get('PUMPD_INTERVAL', 3600)
+PUMPD_SECONDS = os.environ.get('PUMPD_SECONDS', 30)
 
 
-class PumpContext(AbstractPumpContext):
-    def __init__(self):
-        super().__init__()
-        self.relay = automationhat.relay.one
+class Pump:
+    def __init__(self, relay=None):
+        if not relay:
+            relay = automationhat.relay.one
+        self._relay = relay
 
-    async def activate(self):
-        self.relay.on()
-        logger.info('emit (broadcast): pump_started')
-        await sio.emit('pump_started')
+    def on(self):
+        self._relay.on()
+        automationhat.light.power.on()
+        print('PUMP ON')
 
-    async def deactivate(self):
-        self.relay.off()
-        logger.info('emit (broadcast): pump_stopped')
-        await sio.emit('pump_stopped')
-
-
-switch = FloatSwitch(gpio=20)
-pump = Pump(PumpContext())
+    def off(self):
+        self._relay.off()
+        automationhat.light.power.off()
+        print('PUMP OFF')
 
 
-@sio.on('pump_start')
-async def on_pump_start(sid, data):
+class Sensor:
+    def __init__(self, gpio=20):
+        self.gpio = gpio
 
-    async def error(msg):
-        logger.warn(msg)
-        await sio.emit('error', data=msg, room=sid)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(gpio, GPIO.IN)
 
-    try:
-        seconds = int(data['seconds'])
-        logger.info('event (sid=%s): pump_start (%d %s)', sid, seconds, 'second' if seconds == 1 else 'seconds')
-        if seconds < 0:
-            await error('a negative value for "seconds" parameter is not allowed (seconds={})'.format(seconds))
-            return
-    except (TypeError, ValueError):
-        if data['seconds'] is None:
-            logger.info('event (sid=%s): pump_start (client has not specified how many seconds)', sid)
-            seconds = None
+    def is_low(self):
+        return not self.is_high()
+
+    def is_high(self):
+        return not self._read()
+
+    def _read(self):
+        return GPIO.input(self.gpio)
+
+
+class Sprinkler:
+    def __init__(self, loop: asyncio.AbstractEventLoop):
+        self._pump = Pump()
+        self._sensor = Sensor()
+        self._loop = loop
+        self._interval = PUMPD_INTERVAL
+        self._seconds = PUMPD_SECONDS
+        self._done = asyncio.Event()
+
+    def _start(self):
+        if self._sensor.is_high():
+            self._watering(self._seconds)
         else:
-            await error('"seconds" parameter must be an integer (seconds={})'.format(data['seconds']))
-            return
-    except KeyError:
-        seconds = None
+            logger.info('no water!')
+            self._idle()
 
-    if switch.is_high():
-        await pump.start(seconds=seconds)
-    else:
-        logger.info('emit (sid=%s): no_water', sid)
-        await sio.emit('no_water', room=sid)
+    def _watering(self, seconds):
+        logger.info('watering started (%d seconds)', seconds)
+        self._pump.on()
+        start_time = self._loop.time()
+        handle = self._loop.call_later(seconds, self._stop)
 
-
-@sio.on('pump_stop')
-async def on_pump_stop(sid):
-    logger.info('event (sid=%s): pump_stop', sid)
-    await pump.stop()
-
-
-@sio.on('connect')
-async def on_connect(sid, environ):
-    logger.info('event (sid=%s): connect (%s)', sid, environ['REMOTE_ADDR'])
-
-
-@sio.on('client_ready')
-async def on_client_ready(sid):
-    logger.info('event (sid=%s): client_ready', sid)
-    data = {
-        'water': 'high' if switch.is_high() else 'low',
-        'pump': 'running' if pump.is_running else 'stop'
-    }
-
-    await sio.emit('server_ready', data, room=sid)
-    logger.info('emit (sid={}): server_ready, data={}'.format(sid, data))
-
-
-def install_interrupts():
-    loop = asyncio.get_event_loop()
-
-    async def low():
-        logger.info('emit (broadcast): no_water')
-        await sio.emit('no_water')
-        await pump.stop()
-
-    async def high():
-        logger.info('emit (broadcast): water_ok')
-        await sio.emit('water_ok')
-
-    def switch_cb(_):
-        if switch.is_low():
-            asyncio.run_coroutine_threadsafe(low(), loop)
+        def empty_bucket():
+            logger.info('the water is over!')
+            GPIO.remove_event_detect(self._sensor.gpio)
             automationhat.light.warn.on()
-        else:
-            asyncio.run_coroutine_threadsafe(high(), loop)
-            automationhat.light.warn.off()
+            self._pump.off()
+            handle.cancel()
+            elapsed = self._loop.time() - start_time
+            self._waiting(seconds - elapsed)
 
-    GPIO.add_event_detect(switch.gpio, GPIO.BOTH, callback=switch_cb)
+        GPIO.add_event_detect(
+            self._sensor.gpio, GPIO.RISING, bouncetime=500,
+            callback=lambda _:
+                self._loop.call_soon_threadsafe(empty_bucket))
+
+    def _waiting(self, remaining):
+        logger.info('waiting water...')
+
+        def full_bucket():
+            logger.info('watering resumed')
+            GPIO.remove_event_detect(self._sensor.gpio)
+            automationhat.light.warn.off()
+            self._watering(remaining)
+
+        GPIO.add_event_detect(
+            self._sensor.gpio, GPIO.FALLING, bouncetime=500,
+            callback=lambda _:
+                self._loop.call_soon_threadsafe(full_bucket))
+
+    def _stop(self):
+        logger.info('watering finished')
+        GPIO.remove_event_detect(self._sensor.gpio)
+        self._pump.off()
+        self._idle()
+
+    def _idle(self):
+        self._loop.call_later(self._interval, self._start)
+
+    def run(self):
+        logger.info('sprinkler started')
+        self._idle()
 
 
 def main():
-    install_interrupts()
-    app = web.Application()
-    sio.attach(app)
-    web.run_app(app, port=5000)
+    loop = asyncio.get_event_loop()
+    sprinkler = Sprinkler(loop)
+    sprinkler.run()
+    loop.run_forever()
 
 
 if __name__ == '__main__':
